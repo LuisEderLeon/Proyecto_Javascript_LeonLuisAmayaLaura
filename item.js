3aefaced0a7d20b201b8a01a86675e3a868e9338
LuisEderLeon <luiseder.leonvesga@gmail.com>
2024-08-21 15:40:12 -0500

    feat: :rocket: Item fetches

--- item.js
+++ item.js
@@ -5,7 +5,28 @@ let promise = fetch(`https://swapi.dev/api/${type}/${id}`).then((value) => value
 promise.then((value) => {
     document.querySelector("h1").textContent = value.name;
     let paragraph = document.querySelector("p");
+
     for(key in value){
-        paragraph.innerHTML += `${key} - ${value[key]}<br><br>`
+        paragraph.innerHTML += `<hr>${key.charAt(0).toUpperCase() + key.slice(1)}: `
+        if (Array.isArray(value[key])){
+            let saveKey = key.charAt(0).toUpperCase() + key.slice(1) + ": ";
+            value[key].forEach(element => {
+                let content = fetch(element).then((response) => response.json());
+                content.then((data) => {
+                    paragraph.innerHTML = paragraph.innerHTML.slice(0, (paragraph.innerHTML.indexOf(saveKey) + saveKey.length)) + `<br>* ${data.name || data.title}` + paragraph.innerHTML.slice(paragraph.innerHTML.indexOf(saveKey) + saveKey.length)
+                })
+            });
+        }else {
+            if (/^https:\/\/swapi/.test(value[key]) && key != "url"){
+                let content = fetch(value[key]).then((response) => response.json());
+                let saveKey = key.charAt(0).toUpperCase() + key.slice(1) + ": ";
+                content.then((data) => {
+                    paragraph.innerHTML = paragraph.innerHTML.slice(0, (paragraph.innerHTML.indexOf(saveKey) + saveKey.length)) + `${data.name || data.title}` + paragraph.innerHTML.slice(paragraph.innerHTML.indexOf(saveKey) + saveKey.length)
+                });
+            } else{
+                paragraph.innerHTML += `${value[key]}`
+            }
+        }
     }
+    paragraph.innerHTML = paragraph.innerHTML.slice(0, (paragraph.innerHTML.indexOf("<hr>Created")));
 });
