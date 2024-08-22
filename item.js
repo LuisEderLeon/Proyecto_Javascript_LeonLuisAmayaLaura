31924190f8e049caa9e10a67dc13abbda82d1ad9
LuisEderLeon <luiseder.leonvesga@gmail.com>
2024-08-21 21:31:29 -0500

    refactor :hammer: Changed how the HTML is created

--- item.js
+++ item.js
@@ -4,29 +4,30 @@ const id = new URLSearchParams(window.location.search).get("id");
 let promise = fetch(`https://swapi.dev/api/${type}/${id}`).then((value) => value.json());
 promise.then((value) => {
     document.querySelector("h1").textContent = value.name;
-    let paragraph = document.querySelector("p");
+    let list = document.querySelector("div");
 
     for(key in value){
-        paragraph.innerHTML += `<hr>${key.charAt(0).toUpperCase() + key.slice(1)}: `
+        let paragraph = document.createElement("p")
+        paragraph.textContent += `${key.charAt(0).toUpperCase() + key.slice(1)}: `
         if (Array.isArray(value[key])){
             let saveKey = key.charAt(0).toUpperCase() + key.slice(1) + ": ";
             value[key].forEach(element => {
                 let content = fetch(element).then((response) => response.json());
                 content.then((data) => {
-                    paragraph.innerHTML = paragraph.innerHTML.slice(0, (paragraph.innerHTML.indexOf(saveKey) + saveKey.length)) + `<br>* ${data.name || data.title}` + paragraph.innerHTML.slice(paragraph.innerHTML.indexOf(saveKey) + saveKey.length)
+                    paragraph.innerHTML += `<br>- ${data.name || data.title}`
                 })
             });
         }else {
-            if (/^https:\/\/swapi/.test(value[key]) && key != "url"){
+            if (/^https:\/\/swapi/.test(value[key])){
                 let content = fetch(value[key]).then((response) => response.json());
                 let saveKey = key.charAt(0).toUpperCase() + key.slice(1) + ": ";
                 content.then((data) => {
-                    paragraph.innerHTML = paragraph.innerHTML.slice(0, (paragraph.innerHTML.indexOf(saveKey) + saveKey.length)) + `${data.name || data.title}` + paragraph.innerHTML.slice(paragraph.innerHTML.indexOf(saveKey) + saveKey.length)
+                    paragraph.textContent += `${data.name || data.title}`
                 });
             } else{
                 paragraph.innerHTML += `${value[key]}`
             }
         }
+        list.appendChild(paragraph)
     }
-    paragraph.innerHTML = paragraph.innerHTML.slice(0, (paragraph.innerHTML.indexOf("<hr>Created")));
 });
